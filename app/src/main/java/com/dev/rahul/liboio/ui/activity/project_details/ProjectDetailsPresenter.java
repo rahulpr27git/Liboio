a447783b125e592936fe453c11b13fdc860a035a
rahul <dev.rahulprasad@gmail.com>
2018-02-18 10:44:33 +0530

    Fragment transaction methods added

--- app/src/main/java/com/dev/rahul/liboio/ui/activity/project_details/ProjectDetailsPresenter.java
+++ app/src/main/java/com/dev/rahul/liboio/ui/activity/project_details/ProjectDetailsPresenter.java
@@ -2,6 +2,7 @@ package com.dev.rahul.liboio.ui.activity.project_details;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
 import com.dev.rahul.liboio.ui.base.BasePresenter;
@@ -23,6 +24,9 @@ public class ProjectDetailsPresenter<V extends ProjectDetailsMVP.IProjectDetails
         extends BasePresenter<V> implements ProjectDetailsMVP.IProjectDetailsPresenter<V> {
 
     public static final String TAG = ProjectDetailsPresenter.class.getSimpleName();
+
+    private String LAST_FRAGMENT_TAG = null;
+    private String FRAGMENT_TAG = null;
 
     private ProjectDetailsMVP.IProjectDetailsRepository repository;
 
@@ -65,15 +69,26 @@ public class ProjectDetailsPresenter<V extends ProjectDetailsMVP.IProjectDetails
     public void onTabSelected(int pos, boolean wasSelected) {
         Fragment fragment = repository.getFragment(getBaseView().getBundleData())[pos];
 
-        if (fragment instanceof DetailsFragment)
-            getBaseView().onReplaceFragment(fragment, DetailsFragment.TAG);
-        else if (fragment instanceof DependentsFragment)
-            getBaseView().onReplaceFragment(fragment, DependentsFragment.TAG);
-        else if (fragment instanceof DependenciesFragment)
-            getBaseView().onReplaceFragment(fragment, DependenciesFragment.TAG);
-        else if (fragment instanceof ContributorsFragment)
-            getBaseView().onReplaceFragment(fragment, ContributorsFragment.TAG);
-        else if (fragment instanceof RepositoryFragment)
-            getBaseView().onReplaceFragment(fragment, RepositoryFragment.TAG);
+        if (fragment instanceof DetailsFragment) {
+            FRAGMENT_TAG = DetailsFragment.TAG;
+        } else if (fragment instanceof DependentsFragment) {
+            FRAGMENT_TAG = DependentsFragment.TAG;
+        } else if (fragment instanceof DependenciesFragment) {
+            FRAGMENT_TAG = DependenciesFragment.TAG;
+        } else if (fragment instanceof ContributorsFragment) {
+            FRAGMENT_TAG = ContributorsFragment.TAG;
+        } else if (fragment instanceof RepositoryFragment) {
+            FRAGMENT_TAG = RepositoryFragment.TAG;
+        }
+        if (LAST_FRAGMENT_TAG != null && LAST_FRAGMENT_TAG != FRAGMENT_TAG)
+            getBaseView().onHideFragment(LAST_FRAGMENT_TAG);
+
+        LAST_FRAGMENT_TAG = FRAGMENT_TAG;
+
+        if (fragment.isAdded()) {
+            getBaseView().onShowFragment(fragment);
+            return;
+        }
+        getBaseView().onAddFragment(fragment, FRAGMENT_TAG);
     }
 }
